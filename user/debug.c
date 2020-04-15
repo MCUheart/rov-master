2967d9b79dbae31a92265dad2815326d20bd0c17
zengwangfa <zengwangfa@outlook.com>
2020-04-15 23:58:26 +0800

    :hammer: Change: 优化udp通信结构

--- user/debug.c
+++ user/debug.c
@@ -15,6 +15,7 @@
 #include <unistd.h>
 
 #include <wiringPi.h>
+
 #define ANO_SERVER_PORT 8000
 
 static int sockfd;
@@ -22,12 +23,17 @@ static struct sockaddr_in serverAddr;
 static struct sockaddr_in clientAddr;
 static socklen_t addrLen = sizeof(clientAddr);
 
+/**
+  * @brief  ANO匿名地面站UDP发送函数
+  */
 void ano_udp_send(uint8_t *data_to_send, uint8_t len)
 {
-    if ((sendto(sockfd, data_to_send, len, 0, (struct sockaddr *)&clientAddr, addrLen)) < 0)
-        perror("sendto error");
+    sendto(sockfd, data_to_send, len, 0, (struct sockaddr *)&clientAddr, addrLen);
 }
 
+/**
+  * @brief  接收ANO匿名地面站数据线程
+  */
 void *ano_recv_thread(void *arg)
 {
     uint8_t len;
@@ -38,9 +44,13 @@ void *ano_recv_thread(void *arg)
         len = recvfrom(sockfd, buff, sizeof(buff), 0, (struct sockaddr *)&clientAddr, &addrLen);
         ANO_DT_Data_Receive_Anl(buff, len); //数据解析
     }
+    close(sockfd);
     return NULL;
 }
 
+/**
+  * @brief  发送数据至ANO匿名地面站线程
+  */
 void *ano_send_thread(void *arg)
 {
     while (1)
@@ -48,33 +58,38 @@ void *ano_send_thread(void *arg)
         ANO_SEND_StateMachine();
         delay(20);
     }
+    close(sockfd);
     return NULL;
 }
 
-int ano_udp_server_init(void)
+/**
+  * @brief  ANO匿名地面站数据通信服务器线程初始化
+  */
+int anoUdp_server_thread_init(void)
 {
     char mesg[50];
     pthread_t send_tid;
     pthread_t recv_tid;
 
-    // 设置端口及IP
+    // 1.创建UDP socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) // SOCK_DGRAM:数据报协议
+        perror("socket error");
+
+    // 2.设置端口及IP
     bzero(&serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET; // 使用IPv4协议
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddr.sin_port = htons(ANO_SERVER_PORT);
 
-    // 创建UDP socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) // SOCK_DGRAM:数据报协议
-        perror("socket error");
-
+    // 3.绑定IP及端口
     if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)))
         perror("bind error");
 
-    // 等待客户端发送数据，获取客户端IP clientAddr
+    // 4.等待客户端发送数据，获取客户端信息后，才能进行通信
     if (recvfrom(sockfd, mesg, sizeof(mesg), 0, (struct sockaddr *)&clientAddr, &addrLen) < 0)
         perror("recvfrom error");
 
-    log_i("ano link [%s:%d]", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port)); //打印消息发送方的IP与PORT
+    log_i("ano link [%s:%d]", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port)); // 打印消息发送方的IP与PORT
 
     pthread_create(&send_tid, NULL, ano_send_thread, NULL);
     pthread_detach(send_tid);
